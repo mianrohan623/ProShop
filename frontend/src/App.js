4e71d6cb86def565c6ff01d300050a017421c7a3
Muhammad Rohan <164128040+mianrohan623@users.noreply.github.com>
2024-10-22 13:36:38 +0500

    Update Ui and add header and Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Container } from "react-bootstrap";
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <div>Welcome to ProShop</div>
+    <>
+      <Header />
+      <Container>
+        <div>Welcome to ProShop</div>
+      </Container>
+      <Footer />
+    </>
   )
 }
 
